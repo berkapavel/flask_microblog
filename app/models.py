ec10c24a9bd402ee379ce6e139ab201e41e41ad7
berkić <berka.pavel@gmail.com>
2020-02-01 22:00:51 +0100

    Chapter 5: User Logins
    create registration of user

--- app/models.py
+++ app/models.py
@@ -13,6 +13,10 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
