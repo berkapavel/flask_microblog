a0353412644f71eacadfdb6aff004339a9802654
berkić <berka.pavel@gmail.com>
2020-02-01 22:33:31 +0100

    Chapter 6: Profile Page and Avatars
    add avatar to user detail

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login
+from datetime import datetime
 from flask_login import UserMixin
+from hashlib import md5
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(UserMixin, db.Model):
@@ -22,6 +23,12 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size
+        )
 
 
 @login.user_loader
