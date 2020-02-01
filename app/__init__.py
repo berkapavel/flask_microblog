f60b859d59fe444c074e5bd2c7aae1c70eaf6508
berkić <berka.pavel@gmail.com>
2020-02-01 21:29:44 +0100

    Chapter 5: User Logins
    add login, redirect etc.

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,18 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # flask configs
 app = Flask(__name__)
 app.config.from_object(Config)
+# db config
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+# login manager
+login = LoginManager(app)
+login.login_view = 'login'
 
-# app imports
+# app router
 from app import routes
 
