cfb3f79848844dea6fb99606dee45df0fc70291b
berkić <berka.pavel@gmail.com>
2020-02-01 08:54:55 +0100

    Chapter 4: Database
    add sqlite support with configuration

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'something-different'
+    # configuration for forms
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fNrW5HdfBXYyxRbSkfa7SCDENhywxxh9'
+    # SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- migrations/env.py
+++ migrations/env.py
@@ -1,5 +1,4 @@
 from __future__ import with_statement
-from app import models
 
 import logging
 from logging.config import fileConfig
