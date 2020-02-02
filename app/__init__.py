ad11147036abf8275b82832fc7dc746a31716699
berkić <berka.pavel@gmail.com>
2020-02-02 21:49:25 +0100

    Chapter 7: Error Handling
    save errors into file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-import logging
+import logging, os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 # flask configs
 app = Flask(__name__)
@@ -21,6 +21,7 @@ from app import routes, models, errors
 
 # semd errors by email
 if not app.debug:
+    # set sending by e-mail
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -36,3 +37,20 @@ if not app.debug:
         )
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    # set save into logs file
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler(
+        'logs/blog-errors.log',
+        maxBytes=10240,
+        backupCount=10
+    )
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Blog startup')
